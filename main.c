605ccbe0a0e561cce1924b3ab126baa7d93ee1ec
rajah <pierre.tonthat@free.fr>
2025-12-04 02:35:39 +0100

    code cleaning

--- main.c
+++ main.c
@@ -18,15 +18,15 @@
 
 /* structures */
 
-typedef struct PngLdgBuffer {
+typedef struct png_mem_file {
   uint8_t *data;
   int size;
   int offset;
-} PngLdgBuffer;
+} png_mem_file;
 
 /* global variables */
 
-static PngLdgBuffer buffer;
+static png_mem_file png_mf;
 
 static png_structp png_ptr;
 static png_infop   info_ptr;
@@ -39,25 +39,25 @@ static png_uint_32 image_rowbytes;
 
 static void pngldg_write(png_structp png_ptr, png_bytep data, png_size_t count)
 {
-  PngLdgBuffer *buf = (PngLdgBuffer *) png_get_io_ptr(png_ptr);
+  png_mem_file *mf = (png_mem_file *) png_get_io_ptr(png_ptr);
     
   uint32_t new_size;
    
-  if (buf->offset + count > buf->size)
+  if (mf->offset + count > mf->size)
   {
-    new_size = 2 * buf->size;
+    new_size = 2 * mf->size;
       
-    if (buf->offset + count > new_size) { new_size = (((buf->offset + count + 15) >> 4) << 4); }
+    if (mf->offset + count > new_size) { new_size = (((mf->offset + count + 15) >> 4) << 4); }
       
-    buf->data = realloc(buf->data, new_size);
+    mf->data = realloc(mf->data, new_size);
       
-    if (buf->data == NULL) { return; }
+    if (mf->data == NULL) { return; }
       
-    buf->size = new_size;
+    mf->size = new_size;
   }
     
-  memcpy(buf->data + buf->offset, data, count);
-  buf->offset += count;
+  memcpy(mf->data + mf->offset, data, count);
+  mf->offset += count;
 }
 void pngldg_flush(png_structp png_ptr) { }
 
@@ -72,14 +72,15 @@ int32_t CDECL pngenc_open(uint32_t width, uint32_t height, uint32_t depth, uint3
 {
   int size = (width * height) + 1024 + 7; size &= ~7;
   
-  buffer.data = malloc(size);
-  buffer.size = size;
-  buffer.offset = 0;
+  png_mf.data = malloc(size);
+  png_mf.size = size;
+  png_mf.offset = 0;
 
-  if (buffer.data == NULL) { return PNG_ERROR; }
+  if (png_mf.data == NULL) { return PNG_ERROR; }
 
   png_ptr  = png_create_write_struct_2(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL, NULL, pngldg_malloc, pngldg_free);
   info_ptr = png_create_info_struct(png_ptr);
+  
   frame_count = frames_count;
   frame_idx = 0;
   image_height = height;
@@ -88,7 +89,7 @@ int32_t CDECL pngenc_open(uint32_t width, uint32_t height, uint32_t depth, uint3
   {
     if (setjmp(png_jmpbuf(png_ptr))) { png_destroy_write_struct(&png_ptr, &info_ptr); return PNG_ERROR; }
     
-    png_set_write_fn(png_ptr, &buffer, pngldg_write, pngldg_flush);
+    png_set_write_fn(png_ptr, &png_mf, pngldg_write, pngldg_flush);
     png_set_compression_level(png_ptr, 9);
     png_set_IHDR(png_ptr, info_ptr, width, height, depth, colortype, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
     
@@ -138,18 +139,18 @@ int32_t CDECL pngenc_add_image(uint32_t left, uint32_t top, uint32_t width, uint
 
 int32_t CDECL pngenc_write() { if (png_ptr && info_ptr) { png_write_end(png_ptr, info_ptr); return PNG_OK; } return PNG_ERROR; }
 
-uint8_t* CDECL pngenc_get_filedata() { return buffer.data; }
-uint32_t CDECL pngenc_get_filesize() { return buffer.offset; }
+uint8_t* CDECL pngenc_get_filedata() { return png_mf.data; }
+uint32_t CDECL pngenc_get_filesize() { return png_mf.offset; }
 
 int32_t CDECL pngenc_close()
 {
   png_destroy_write_struct(&png_ptr, &info_ptr);
 
-  free(buffer.data);
+  free(png_mf.data);
 
-  buffer.data = NULL;
-  buffer.size = 0;
-  buffer.offset = 0;
+  png_mf.data = NULL;
+  png_mf.size = 0;
+  png_mf.offset = 0;
 
   frame_count = 0;
   frame_idx = 0;
