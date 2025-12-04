7c23c85c5b305cb9b4d81b8deb530659e6cfcf4a
rajah <pierre.tonthat@free.fr>
2025-12-04 14:02:27 +0100

    renamed vars

--- main.c
+++ main.c
@@ -26,10 +26,10 @@ typedef struct png_mem_file {
 
 /* global variables */
 
-static png_mem_file png_mf;
+png_mem_file png_mf;
+png_structp  png_write;
+png_infop    img_info;
 
-static png_structp png_ptr;
-static png_infop   info_ptr;
 static png_uint_32 frame_count;
 static png_uint_32 frame_idx;
 static png_uint_32 image_height;
@@ -78,26 +78,26 @@ int32_t CDECL pngenc_open(uint32_t width, uint32_t height, uint32_t depth, uint3
 
   if (png_mf.data == NULL) { return PNG_ERROR; }
 
-  png_ptr  = png_create_write_struct_2(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL, NULL, pngldg_malloc, pngldg_free);
-  info_ptr = png_create_info_struct(png_ptr);
+  png_write  = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);//, NULL, pngldg_malloc, pngldg_free);
+  img_info = png_create_info_struct(png_write);
   
   frame_count = frames_count;
   frame_idx = 0;
   image_height = height;
 
-  if (png_ptr && info_ptr)
+  if (png_write && img_info)
   {
-    if (setjmp(png_jmpbuf(png_ptr))) { png_destroy_write_struct(&png_ptr, &info_ptr); return PNG_ERROR; }
+    if (setjmp(png_jmpbuf(png_write))) { png_destroy_write_struct(&png_write, &img_info); return PNG_ERROR; }
     
-    png_set_write_fn(png_ptr, &png_mf, pngldg_write, pngldg_flush);
-    png_set_compression_level(png_ptr, 9);
-    png_set_IHDR(png_ptr, info_ptr, width, height, depth, colortype, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_set_write_fn(png_write, &png_mf, pngldg_write, pngldg_flush);
+    png_set_compression_level(png_write, 9);
+    png_set_IHDR(png_write, img_info, width, height, depth, colortype, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
     
-    if (frames_count > 1) { png_set_acTL(png_ptr, info_ptr, frames_count, plays_count); }
+    if (frames_count > 1) { png_set_acTL(png_write, img_info, frames_count, plays_count); }
     
-    png_write_info(png_ptr, info_ptr);
+    png_write_info(png_write, img_info);
     
-    image_rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+    image_rowbytes = png_get_rowbytes(png_write, img_info);
 
     return PNG_OK;
   }
@@ -107,7 +107,7 @@ int32_t CDECL pngenc_open(uint32_t width, uint32_t height, uint32_t depth, uint3
 
 int32_t CDECL pngenc_add_image(uint32_t left, uint32_t top, uint32_t width, uint32_t height, uint32_t delay_num, uint32_t delay_den, uint32_t dispose_op, uint32_t blend_op, uint8_t *p_frame)
 {
-  if (png_ptr && info_ptr)
+  if (png_write && img_info)
   {
     png_bytepp rows = (png_bytepp)malloc(image_height * sizeof(png_bytep));
 
@@ -119,13 +119,13 @@ int32_t CDECL pngenc_add_image(uint32_t left, uint32_t top, uint32_t width, uint
 
       if (frame_count == 1)
       {
-        png_write_image(png_ptr, rows);
+        png_write_image(png_write, rows);
       }
       else
       {
-        png_write_frame_head(png_ptr, info_ptr, NULL, width, height, left, top, delay_num, delay_den, dispose_op, blend_op);
-        png_write_image(png_ptr, rows);
-        png_write_frame_tail(png_ptr, info_ptr);
+        png_write_frame_head(png_write, img_info, NULL, width, height, left, top, delay_num, delay_den, dispose_op, blend_op);
+        png_write_image(png_write, rows);
+        png_write_frame_tail(png_write, img_info);
       }
   
       frame_idx++;
@@ -137,15 +137,13 @@ int32_t CDECL pngenc_add_image(uint32_t left, uint32_t top, uint32_t width, uint
   return PNG_ERROR;
 }
 
-int32_t CDECL pngenc_write() { if (png_ptr && info_ptr) { png_write_end(png_ptr, info_ptr); return PNG_OK; } return PNG_ERROR; }
+int32_t CDECL pngenc_write() { if (png_write && img_info) { png_write_end(png_write, img_info); return PNG_OK; } return PNG_ERROR; }
 
 uint8_t* CDECL pngenc_get_filedata() { return png_mf.data; }
 uint32_t CDECL pngenc_get_filesize() { return png_mf.offset; }
 
 int32_t CDECL pngenc_close()
 {
-  png_destroy_write_struct(&png_ptr, &info_ptr);
-
   free(png_mf.data);
 
   png_mf.data = NULL;
@@ -157,7 +155,14 @@ int32_t CDECL pngenc_close()
   image_height = 0;
   image_rowbytes = 0;
 
-  return PNG_OK;
+  if (png_write && img_info)
+  {
+    png_write_end(png_write, img_info);
+    png_destroy_write_struct(&png_write, &img_info);
+
+    return PNG_OK;
+  }
+  return PNG_ERROR;
 }
 
 /* populate functions list and info for the LDG */
